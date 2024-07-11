e331474b00d8d038ad54dab40c979103d9cd488f
Ankur Satyarthi <sguibnkawin0000000001@gmail.com>
2024-07-11 14:32:09 -0700

    updated ui

--- sales_analysis_app.jl
+++ sales_analysis_app.jl
@@ -1,9 +1,18 @@
 using Gtk
-win=GtkWindow("Sales Analysis App",400,200)
-b=GtkButton("Button")
-push!(win,b)
-function on_button_clicked(w)
-    println("The button has been clicked")    
-end
-signal_connect(on_button_clicked, b, "clicked")
+win=GtkWindow("Sales Analysis App")
+hbox=GtkBox(:h)
+push!(win,hbox)
+cancel=GtkButton("Cancel")
+ok=GtkButton("OK")
+push!(hbox,cancel)
+push!(hbox,ok)
+# function on_button_clicked(w)
+#     println("The button has been clicked")    
+# end
+# signal_connect(on_button_clicked, b, "clicked")
+length(hbox)
+get_gtk_property(hbox[1],:label,String)
+get_gtk_property(hbox[2],:label,String)
+
+
 showall(win)
