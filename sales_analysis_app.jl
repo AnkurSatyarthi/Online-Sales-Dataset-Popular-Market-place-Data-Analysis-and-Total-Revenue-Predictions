683dbc7385bb373a513a53cc46dd82a9bb479ff9
Ankur Satyarthi <sguibnkawin0000000001@gmail.com>
2024-07-11 14:40:34 -0700

    UI Changed #1

--- sales_analysis_app.jl
+++ sales_analysis_app.jl
@@ -13,6 +13,21 @@ push!(hbox,ok)
 length(hbox)
 get_gtk_property(hbox[1],:label,String)
 get_gtk_property(hbox[2],:label,String)
+set_gtk_property!(hbox,:expand,ok,true)
+set_gtk_property!(hbox,:spacing,10)
+destroy(hbox)
+ok=GtkButton("OK")
+cancel=GtkButton("Cancel")
+hbox=GtkButtonBox(:h)
+push!(win,hbox)
+push!(hbox,cancel)
+push!(hbox,ok)
+
+
+
+
+
+
 
 
 showall(win)
