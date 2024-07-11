4f602242a7ca71b7b63ade64d61e2f92553d8620
Ankur Satyarthi <sguibnkawin0000000001@gmail.com>
2024-07-11 15:28:17 -0700

    updated ui.jl #2

--- OnlineSalesDataAnalysis/ui.jl
+++ OnlineSalesDataAnalysis/ui.jl
@@ -1,4 +1,21 @@
 heading("Online Data Analysis App")
-row([ ])
+row([
+    cell(class="col-md-12",
+        [
+            uploader(multiple=true,
+                accept=".csv",
+                maxfilesize=1024*1024*1, #bytes
+                maxfiles=3,
+                autoupload=true,
+                hideuploadbtn=true,
+                label="Upload Datasets",
+                nothumnails="true",
+                style="max-width:95%;width:95%;margin:0 auto;",
+                
+                @on("rejected",:rejected),
+                @on("uploaded",:uploaded)
+                )
+        ])
+])
 row([ ])
 row([ ])
