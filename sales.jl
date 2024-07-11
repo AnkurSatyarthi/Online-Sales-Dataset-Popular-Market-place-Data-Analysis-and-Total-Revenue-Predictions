de80bb7af7cccd987c73144aa4ef8b72d43f8b03
Ankur Satyarthi <sguibnkawin0000000001@gmail.com>
2024-07-11 13:55:49 -0700

    updated sales.jl

--- sales.jl
+++ sales.jl
@@ -1,2 +1,7 @@
-using DataFrames,CSV,TypedTables,Plots
+using DataFrames,CSV,TypedTables,Statistics,Plots
 sales=DataFrame(CSV.File("data/Online Sales Data.csv"))
+first(sales)
+last(sales)
+describe(sales)
+show(sales,allcols=true)
+mean(sales."Units Sold")
